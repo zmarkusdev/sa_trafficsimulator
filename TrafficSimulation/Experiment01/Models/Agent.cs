f7fd0b231c48cac2b7568f5dd3900c9a9f709bb7
TED <fhs37390@fh-salzburg.ac.at>
2017-04-19 19:20:58 +0200

    GUI Experimental Project added to test some basic functions including MVVM for WPF

---
+++ TrafficSimulation/Experiment01/Models/Agent.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technics;
+
+namespace Experiment01.Models
+{
+    class Agent : Model
+    {
+        private int _Xpos;
+        public int Xpos
+        {
+            get
+            {
+                return _Xpos;
+            }
+
+            set
+            {
+                if (_Xpos != value)
+                {
+                    _Xpos = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int _Ypos;
+        public int Ypos
+        {
+            get
+            {
+                return _Ypos;
+            }
+
+            set
+            {
+                if (_Ypos != value)
+                {
+                    _Ypos = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int _Rotation;
+        public int Rotation
+        {
+            get
+            {
+                return _Rotation;
+            }
+
+            set
+            {
+                if (_Rotation != value)
+                {
+                    _Rotation = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string _ImagePath;
+        public string ImagePath
+        {
+            get
+            {
+                return _ImagePath;
+            }
+
+            set
+            {
+                if (_ImagePath != value)
+                {
+                    _ImagePath = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int _Width;
+        public int Width
+        {
+            get
+            {
+                return _Width;
+            }
+
+            set
+            {
+                if (_Width != value)
+                {
+                    _Width = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int _Height;
+        public int Height
+        {
+            get
+            {
+                return _Height;
+            }
+
+            set
+            {
+                if (_Height != value)
+                {
+                    _Height = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public Agent(int x, int y, int rot, int type)
+        {
+            Xpos = x;
+            Ypos = y;
+            Rotation = rot;
+
+            switch (type)
+            {
+                default:
+                    ImagePath = Properties.Resources.Car01.;
+            }
+
+            Image img = Image.FromFile(ImagePath);
+            Width = img.Width;
+            Height = img.Height;
+        }
+
+
+
+
+    }
+}
---
+++ TrafficSimulation/Experiment01/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Experiment01.Models;
+using Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Experiment01.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private DispatcherTimer timer;
+        private IAgentRepository Agents;
+
+        public MainWindowModel GUI { get; set; }
+
+        public MainWindowViewModel()
+        {
+            /// Create Gui Variables (Bindings to GUI)
+            GUI = new MainWindowModel();
+
+            /// Create AgentRepository with Factory
+            Agents = AgentRepositoryFactory.CreateRepository();
+
+            /// Set up the Timer for refreshing the GUI
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            /// Start the GUI actualization timer
+            timer.Start();
+        }
+
+        /// <summary>
+        /// This Timer Tick event refreshs the gui at every usage
+        /// This is done by actualize all objects from the datamodel to the internal models
+        /// </summary>
+        /// <param name="sender">not used</param>
+        /// <param name="e">not used</param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            IEnumerable ags = Agents.GetAllAgents();
+            foreach (Agent ag in ags)
+            {
+                ag.
+            }
+
+
+
+        }
+    }
+}
