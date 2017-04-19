dbb5747e7b7e2593e54b1d27a68723d44eede199
Walter <walter.bernhofer@yahoo.de>
2017-04-19 20:25:33 +0200

    Added Service directory

---
+++ TrafficSimulation/DataBridge/Services/MockedAgentService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datamodel;
+
+namespace DataBridge.Services
+{
+    class MockedAgentService : Repositories.IAgentRepository
+    {
+        public Agent Create(Agent agent)
+        {
+            Agent newAgent = new Agent();
+            newAgent.Id = 123;
+            newAgent.Acceleration = agent.Acceleration;
+            newAgent.CurrentVelocity = agent.CurrentVelocity;
+            newAgent.Deceleration = agent.Deceleration;
+            newAgent.MaxVelocity = agent.MaxVelocity;
+            newAgent.PositionId = agent.PositionId;
+            return newAgent;
+        }
+
+        public void Delete(Agent agent)
+        {
+            return;
+        }
+
+        public Agent GetAgent(int agentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Agent> GetAgentsForPositionIds(int positionIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Agent> GetAllAgents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Agent Update(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
