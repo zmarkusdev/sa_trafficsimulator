cc4bb0be4587925a241fcacff761c0182cf737fe
Walter <walter.bernhofer@yahoo.de>
2017-04-28 17:04:17 +0200

    Added Controller directory and StreamConverter

---
+++ TrafficSimulation/DataAccessLayer/Controller/StreamConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace DataAccessLayer.Controller
+{
+    class StreamConverter
+    {
+
+        public static string convertToJson(Object obj)
+        {
+            return new JavaScriptSerializer().Serialize(obj);
+        }
+
+        public static Object convertFromJson(String str)
+        {
+            return new JavaScriptSerializer().Deserialize(str);
+        }
+    }
+}
