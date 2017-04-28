e7600e13ac9b41ea90136d0e92419afb5f5eb98b
TED <fhs37390@fh-salzburg.ac.at>
2017-04-28 19:06:56 +0200

    Positioning of Positions, Edges and Agents mainly finished

---
+++ TrafficSimulation/SimulationUserInterface/Models/Repositorys/EdgeRepository.cs
@@ -0,0 +1,105 @@
+﻿using Datamodel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SimulationUserInterface.Models
+{
+    /// <summary>
+    /// Repository of all Edges which should be shown in the User Interface
+    /// </summary>
+    public class EdgeRepository
+    {
+        #region ----- Variables 
+
+        /// <summary>
+        /// GUI X scale factor for repositioning and resizing the positions
+        /// </summary>
+        private double XScaleFactor { get; set; }
+       
+        /// <summary>
+        /// GUI Y scale factor for repositioning and resizing the positions
+        /// </summary>
+        private double YScaleFactor { get; set; }
+
+        /// <summary>
+        /// Collection of edge elements which should be shown in the User Interface
+        /// </summary>
+        public ObservableCollection<EdgeModel> MapEdges { get; set; }
+
+        #endregion
+
+
+
+        #region ----- Constructor
+
+        /// <summary>
+        /// Default Constructor which has to initialize the ObservableCollection of EdgeModels
+        /// </summary>
+        public EdgeRepository()
+        {
+            MapEdges = new ObservableCollection<EdgeModel>();
+        }
+
+        #endregion
+
+
+
+        #region ----- Public functions
+
+        /// <summary>
+        /// Update the scales for repositioning and resizing the edge elements
+        /// </summary>
+        /// <param name="xScaleFactor">X</param>
+        /// <param name="yScaleFactor">Y</param>
+        public void SetScaleFactors(double xScaleFactor, double yScaleFactor)
+        {
+            try
+            {
+                /// Set the scale factors (set to 1 if they are zero to prevent a possible division through 0)
+                XScaleFactor = (xScaleFactor == 0.0) ? 1 : xScaleFactor;
+                YScaleFactor = (yScaleFactor == 0.0) ? 1 : yScaleFactor;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes all existing EdgeModels and creates new ones with the given 
+        /// position and edge information
+        /// </summary>
+        /// <param name="positions">Positions from the DataBridge</param>
+        /// <param name="edges">Edges from the DataBridge</param>
+        public void DrawEdges(IEnumerable<Position> positions, IEnumerable<Edge> edges)
+        {
+            try
+            {
+                /// Delete all old Edges
+                MapEdges.Clear();
+
+                foreach (Edge singleEdge in edges)
+                {
+                    /// Get the start and end position IDs from the used edge
+                    int startPositionId = singleEdge.StartPositionId;
+                    int endPositionId = singleEdge.EndPositionId;
+
+                    /// Get the used positions from the list by its ID
+                    Position startposition = positions.Where(var => var.Id == startPositionId).ToList().First();
+                    Position endposition = positions.Where(var => var.Id == endPositionId).ToList().First();
+                    
+                    /// Add a new edge with created information
+                    MapEdges.Add(new EdgeModel(startposition.X, startposition.Y, endposition.X, endposition.Y, XScaleFactor, YScaleFactor));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+
+        #endregion
+    } // Class
+} // Namespace
--- TrafficSimulation/SimulationUserInterface/Technics/Base64ImageConverter.cs
+++ TrafficSimulation/SimulationUserInterface/Technics/Base64ImageConverter.cs
@@ -5,24 +5,55 @@ using System.Windows.Media.Imaging;
 
 namespace SimulationUserInterface.Technics
 {
+    /// <summary>
+    /// Converts the Base64 in a XAML usable format for Image tag
+    /// </summary>
     public class Base64ImageConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a Base64 stream into a BitmapImage that can be used for XAML Image
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            string s = value as string;
+            try
+            {
+                string s = value as string;
 
-            if (s == null)
-                return null;
+                /// Security check
+                if (s == null)
+                {
+                    return new BitmapImage();
+                }
 
-            BitmapImage bi = new BitmapImage();
+                /// Create an Image
+                BitmapImage bi = new BitmapImage();
 
-            bi.BeginInit();
-            bi.StreamSource = new MemoryStream(System.Convert.FromBase64String(s));
-            bi.EndInit();
+                /// Translate the stream into an Image
+                bi.BeginInit();
+                bi.StreamSource = new MemoryStream(System.Convert.FromBase64String(s));
+                bi.EndInit();
 
-            return bi;
+                return bi;
+            }
+            catch
+            {
+                return new BitmapImage();
+            }
         }
 
+        /// <summary>
+        /// Convert back is not implemented due to no useage
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
