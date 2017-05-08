1dc964d4d89453cd77a47fc4f1b7b70899ddd1e2
David Schoeninger <fhs40899@fhs-salzburg.ac.at>
2017-05-08 21:46:11 +0200

    Created project for VehicleHandoverLibrary

---
+++ VehicleHandoverLibrary/HandoverTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandoverTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new VehicleHandoverLibrary.VehicleHandoverService();
+        }
+    }
+}
---
+++ VehicleHandoverLibrary/VehicleHandoverLibrary/VehicleHandoverService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Amazon.Runtime;
+
+namespace VehicleHandoverLibrary
+{
+    public class VehicleHandoverService
+    {
+
+        AWSCredentials awsCredentials;
+        AmazonSQSClient sqsClient;
+        String SQS_URL = "https://sqs.us-east-2.amazonaws.com/047952185359/vehicle-handover-01.fifo";
+
+        public VehicleHandoverService()
+        {
+            awsCredentials = new BasicAWSCredentials("AKIAJAXVEF37WTBOI5RA", "K78sgjKL3XRD/T4T8/8yubFroIQ8PtvZBQ2yq5r+");
+            sqsClient = new AmazonSQSClient(awsCredentials, Amazon.RegionEndpoint.USEast2);
+
+            sendMessage("I bims!");
+            var messages = receiveMessage();
+            Console.WriteLine(messages);
+        }
+
+        private void sendMessage(String message)
+        {
+            var sendMessageRequest = new SendMessageRequest(SQS_URL, message);
+            sendMessageRequest.MessageGroupId = "1";
+            sendMessageRequest.MessageDeduplicationId = Guid.NewGuid().ToString();
+            sqsClient.SendMessage(sendMessageRequest);
+        }
+
+        private String receiveMessage()
+        {
+            String response = "";
+            var receiveMessageRequest = new ReceiveMessageRequest(SQS_URL);
+            var receiveMessageResponse = sqsClient.ReceiveMessage(receiveMessageRequest);
+
+            foreach (var message in receiveMessageResponse.Messages)
+            {
+                response += "[" + message.Body + "]";
+
+                // Delete it
+                var deleteMessageRequest = new DeleteMessageRequest(SQS_URL, message.ReceiptHandle);
+                sqsClient.DeleteMessage(deleteMessageRequest);
+            }
+
+            return response;
+        }
+
+    }
+}
