de54fce1839a308449294a7ba2bcc3c439368884
David Schoeninger <fhs40899@fhs-salzburg.ac.at>
2017-05-08 21:53:40 +0200

    Documented SQS Options

--- VehicleHandoverLibrary/VehicleHandoverLibrary/VehicleHandoverService.cs
+++ VehicleHandoverLibrary/VehicleHandoverLibrary/VehicleHandoverService.cs
@@ -8,6 +8,14 @@ using Amazon.SQS;
 using Amazon.SQS.Model;
 using Amazon.Runtime;
 
+/*
+ * QUEUE Options:
+ *  Default Visibility Timeout: 2m
+ *  Message Retention Period: 1m
+ *  Receive Message Wait Time (Long-polling): 20s
+ *  Content-Based Deduplication: false
+ */
+
 namespace VehicleHandoverLibrary
 {
     public class VehicleHandoverService
@@ -16,13 +24,14 @@ namespace VehicleHandoverLibrary
         AWSCredentials awsCredentials;
         AmazonSQSClient sqsClient;
         String SQS_URL = "https://sqs.us-east-2.amazonaws.com/047952185359/vehicle-handover-01.fifo";
+        String MESSAGE_GROUP_ID = "1";
 
         public VehicleHandoverService()
         {
             awsCredentials = new BasicAWSCredentials("AKIAJAXVEF37WTBOI5RA", "K78sgjKL3XRD/T4T8/8yubFroIQ8PtvZBQ2yq5r+");
             sqsClient = new AmazonSQSClient(awsCredentials, Amazon.RegionEndpoint.USEast2);
 
-            sendMessage("I bims!");
+            //sendMessage("I bims!");
             var messages = receiveMessage();
             Console.WriteLine(messages);
         }
@@ -30,7 +39,7 @@ namespace VehicleHandoverLibrary
         private void sendMessage(String message)
         {
             var sendMessageRequest = new SendMessageRequest(SQS_URL, message);
-            sendMessageRequest.MessageGroupId = "1";
+            sendMessageRequest.MessageGroupId = MESSAGE_GROUP_ID;
             sendMessageRequest.MessageDeduplicationId = Guid.NewGuid().ToString();
             sqsClient.SendMessage(sendMessageRequest);
         }
