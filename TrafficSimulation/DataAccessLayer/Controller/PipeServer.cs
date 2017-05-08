614c9540b884cb6f81296499bee4adacd07e7e73
Walter <walter.bernhofer@yahoo.de>
2017-05-08 23:14:32 +0200

    Pipe vereinfacht

--- TrafficSimulation/DataAccessLayer/Controller/PipeServer.cs
+++ TrafficSimulation/DataAccessLayer/Controller/PipeServer.cs
@@ -1,49 +1,38 @@
 ﻿using DataAccessLayer.Services;
 using DataModel.Pipe;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.IO.Pipes;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DataAccessLayer.Controller
 {
-    public abstract class PipeServer
+    public class PipeServer
     {
-        private static int bufferSize = 10;
+        private static int bufferSize = 4096;
         private JsonStreamConverter converter;
         private NamedPipeServerStream _pipeServer;
-        private StringBuilder messageBuilder;
-        private byte[] messageBuffer;
-        private string messageChunk;
+        private StreamReader reader;
+        private StreamWriter writer;
         private IPipeService service;
 
-        public PipeServer(IPipeService service)
+        public PipeServer(string pipeName, IPipeService service)
         {
-            this.service = service;
             this.converter = JsonStreamConverter.getInstance();
-            this.messageBuilder = new StringBuilder();
-            this.messageChunk = string.Empty;
+            reader = new StreamReader(_pipeServer);
+            writer = new StreamWriter(_pipeServer);
+            this.service = service;
+
+            this._pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+
+            connect();
         }
 
-
-        /// <summary>
-        /// Creates a new NamedPipeServerStream 
-        /// </summary>
-        protected void createPipeAndRun(String pipeName)
-        {
-           this._pipeServer = new NamedPipeServerStream(
-               pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous
-               );
-            run();
-        }
 
         /// <summary>
         /// This method begins an asynchronous operation to wait for a client to connect.
         /// </summary>
-        private void run()
+        private void connect()
         {
             try
             {
@@ -66,14 +55,14 @@ namespace DataAccessLayer.Controller
             // Call EndWaitForConnection to complete the connection operation
             _pipeServer.EndWaitForConnection(result);
 
-            beginRead(new Info());
+            beginRead();
         }
 
-        private void beginRead(Info info)
+        private void beginRead()
         {
             try
             {
-                _pipeServer.BeginRead(info.Buffer, 0, bufferSize, EndReadCallBack, info);
+                converter.convertFromJson<PipeDTO>(reader.ReadToEnd());
             }
             catch (Exception ex)
             {
@@ -83,98 +72,10 @@ namespace DataAccessLayer.Controller
             }
         }
 
-        /// <summary>
-        /// This callback is called when the BeginRead operation is completed.
-        /// We can arrive here whether the connection is valid or not
-        /// </summary>
-        private void EndReadCallBack(IAsyncResult result)
-        {
-            var readBytes = _pipeServer.EndRead(result);
-            if (readBytes > 0)
-            {
-                var info = (Info)result.AsyncState;
-
-                // Get the read bytes and append them
-                info.StringBuilder.Append(Encoding.UTF8.GetString(info.Buffer, 0, readBytes));
-
-                if (!_pipeServer.IsMessageComplete) // Message is not complete, continue reading
-                {
-                    beginRead(info);
-                }
-                else // Message is completed
-                {
-                    // Finalize the received string and fire MessageReceivedEvent
-                    service.executeCommand(converter.convertFromJson<PipeDTO>(info.StringBuilder.ToString().TrimEnd('\0')));
-                    // Begin a new reading operation
-                    beginRead(new Info());
-                }
-            }
-            else // When no bytes were read, it can mean that the client have been disconnected
-            {
-                try
-                {
-                    if (_pipeServer.IsConnected)
-                    {
-                        _pipeServer.Disconnect();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.StackTrace);
-                    throw;
-                }
-                finally
-                {
-                    _pipeServer.Close();
-                    _pipeServer.Dispose();
-                }
-            }
-        }
-
         public void write(PipeDTO dto)
         {
-            _pipeServer.WaitForPipeDrain();
-            _pipeServer.BeginWrite(
-                Encoding.UTF8.GetBytes(converter.convertToJson<PipeDTO>(dto)),
-                0,
-                Encoding.UTF8.GetBytes(converter.convertToJson<PipeDTO>(dto)).Length,
-                EndWriteCallBack,
-                _pipeServer
-            );
-        }
-
-        private void EndWriteCallBack(IAsyncResult result)
-        {
-            try
-            {
-                // End the write
-                _pipeServer.EndWrite(result);
-                _pipeServer.Flush();
-            }
-            catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.StackTrace);
-                    throw;
-                }
-                finally
-                {
-                    _pipeServer.Close();
-                    _pipeServer.Dispose();
-                }
-        }
-
-        protected class Info
-        {
-            public readonly byte[] Buffer;
-            public readonly StringBuilder StringBuilder;
-
-            public Info()
-            {
-                Buffer = new byte[bufferSize];
-                StringBuilder = new StringBuilder();
-            }
+            writer.Write(converter.convertToJson<PipeDTO>(dto));
+            writer.Flush();
         }
     }
 
