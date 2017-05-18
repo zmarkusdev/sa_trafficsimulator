c8b7da8cdefaf88963726d1a0a0542a82a095d73
Walter <walter.bernhofer@yahoo.de>
2017-05-18 17:55:54 +0200

    Removed pipe thing and included wcf-pipe functionality

---
+++ TrafficSimulation/DataAccessLayer/Communication/DataAccessService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datamodel;
+using System.ServiceModel;
+
+namespace DataAccessLayer.Communication
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    class DataAccessService : IDataAccessContract
+    {
+        private IAgentDataAccess agentDataAccess;
+
+        public DataAccessService()
+        {
+            agentDataAccess = AgentDataAccessFactory.CreateRepository();
+            agentDataAccess.LoadfromFile("agent");
+        }
+        
+
+        public Agent CreateAgent(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAgent(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Agent GetAgent(int agentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Agent> GetAgentsForPositionIds(int positionIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Agent> GetAllAgents()
+        {
+            return agentDataAccess.ReadAll();
+        }
+
+        public Agent UpdateAgent(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TrafficSimulation/maxTest/Program.cs
+++ TrafficSimulation/maxTest/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DataAccessLayer.Communication;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,25 +14,17 @@ namespace maxTest
     {
         static void Main(string[] args)
         {
-            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream("DataAccess-pipe"))
-            {
-                string message = "";
-                namedPipeClient.Connect();
-                do
-                {
-                    Console.Write("Enter a message to be sent to the server: ");
-                    message = Console.ReadLine();
-                    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                    try
-                    {
-                        namedPipeClient.Write(messageBytes, 0, messageBytes.Length);
-                    }
-                    catch (IOException e)
-                    {
-                        Console.WriteLine("Die Pipe ist verstopft!");
-                    }
-                } while (String.Compare(message, "ende") != 0);
-            }
+            string address = "net.pipe://localhost/ttrafficSim/DataAccessService";
+
+            NetNamedPipeBinding binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            EndpointAddress ep = new EndpointAddress(address);
+            IDataAccessContract channel = ChannelFactory<IDataAccessContract>.CreateChannel(binding, ep);
+
+            var agents = channel.GetAllAgents();
+
+            Console.WriteLine("Client Connected");
+
+
             Console.WriteLine("Fertig");
             Console.ReadKey();
 
