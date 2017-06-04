52d470d758bf13e2c3f1ec508e6a7105a6d68625
Markus Ziegler <mrksziegler@gmail.com>
2017-06-04 17:47:33 +0200

    Log Projekt noch eingecheckt

---
+++ TrafficSimulation/Log/ILog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    public interface ILog
+    {
+        void Debug(string message);
+    }
+}
